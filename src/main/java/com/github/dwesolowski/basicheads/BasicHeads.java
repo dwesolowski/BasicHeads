dc71a31903296b489d4efb4773e7f50bd429d2c5
dwesolowski <daren.wesolowski@icloud.com>
2021-03-13 17:55:39 -0500

    Added configurable messages

--- src/main/java/com/github/dwesolowski/basicheads/BasicHeads.java
+++ src/main/java/com/github/dwesolowski/basicheads/BasicHeads.java
@@ -1,5 +1,7 @@
 package com.github.dwesolowski.basicheads;
 
+import net.md_5.bungee.api.ChatColor;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -35,12 +37,29 @@ public class BasicHeads extends JavaPlugin implements Listener {
                 if (getConfig().getBoolean("useRandom")) {
                     if (shouldDrop) {
                         victim.getLocation().getWorld().dropItemNaturally(victim.getLocation(), skull);
+                        if (checkConfigValue("Messages.ObtainHead")) {
+                            killer.sendMessage(colorize("Messages.ObtainHeadByChance").replace("{VICTIM}", victim.getName()));
+                            victim.sendMessage(colorize("Messages.LostYourHead").replace("{KILLER}", killer.getName()));
+                        }
                     }
 
                 } else {
                     victim.getLocation().getWorld().dropItemNaturally(victim.getLocation(), skull);
+                    if (checkConfigValue("Messages.ObtainHead")) {
+                        killer.sendMessage(colorize("Messages.ObtainHead").replace("{VICTIM}", victim.getName()));
+                        victim.sendMessage(colorize("Messages.LostYourHead").replace("{KILLER}", killer.getName()));
+                    }
                 }
             }
         }
     }
+
+    public String colorize(String str) {
+        return ChatColor.translateAlternateColorCodes('&', getConfig().getString(str));
+    }
+
+    public boolean checkConfigValue(String name) {
+        return !StringUtils.isBlank(getConfig().getString(name));
+    }
 }
+
